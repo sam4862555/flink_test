3cba53ad9f94baa7d0521267539fa9255ff61eee
unknown <870991173@qq.com>
2024-10-01 09:51:32 +0800

    delete ./idea

--- src/main/java/org/example/FlinkSQLFile.java
+++ src/main/java/org/example/FlinkSQLFile.java
@@ -4,18 +4,19 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
-public class Example12 {
+public class FlinkSQLFile{
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().inStreamingMode().build());
-
+        // "'path' = '/home/zuoyuan/flinktutorial1118/src/main/resources/clicks.csv'," +
+        // D:\IDEA_Projects\FlinkTest\src\main\resources\clicks.csv
         streamTableEnvironment
                 .executeSql(
                         "create table clicks (`user` STRING, `url` STRING, " +
                                 "ts TIMESTAMP(3)," +
                                 "WATERMARK FOR ts AS ts - INTERVAL '3' SECONDS) WITH (" +
                                 "'connector' = 'filesystem'," +
-                                "'path' = '/home/zuoyuan/flinktutorial1118/src/main/resources/clicks.csv'," +
+                                "'path' = 'D:\\\\IDEA_Projects\\FlinkTest\\src\\main\\resources\\clicks.csv'," +
                                 "'format' = 'csv'" +
                                 ")"
                 );
