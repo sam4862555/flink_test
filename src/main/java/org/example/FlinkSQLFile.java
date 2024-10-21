c439db774c9097292d3b8e8f33f59e51ba2524a5
孙振华 <sunzhenhua@sunzhenhuadeMacBook-Pro.local>
2024-10-21 18:09:26 +0800

    recent change

--- src/main/java/org/example/FlinkSQLFile.java
+++ src/main/java/org/example/FlinkSQLFile.java
@@ -9,14 +9,14 @@ public class FlinkSQLFile{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().inStreamingMode().build());
         // "'path' = '/home/zuoyuan/flinktutorial1118/src/main/resources/clicks.csv'," +
-        // D:\IDEA_Projects\FlinkTest\src\main\resources\clicks.csv
+        // "'path' = 'D:\\\\IDEA_Projects\\FlinkTest\\src\\main\\resources\\clicks.csv'," +
         streamTableEnvironment
                 .executeSql(
                         "create table clicks (`user` STRING, `url` STRING, " +
                                 "ts TIMESTAMP(3)," +
                                 "WATERMARK FOR ts AS ts - INTERVAL '3' SECONDS) WITH (" +
                                 "'connector' = 'filesystem'," +
-                                "'path' = 'D:\\\\IDEA_Projects\\FlinkTest\\src\\main\\resources\\clicks.csv'," +
+                                "'path' = '/Users/sunzhenhua/IdeaProjects/flink_test/src/main/resources/clicks.csv'," +
                                 "'format' = 'csv'" +
                                 ")"
                 );
---
+++ src/main/java/org/example/StringTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("1".compareTo("2"));
+        System.out.println("3".compareTo("2"));
+    }
+}
