ce14d279fc9367d8174c9b6ad13337d6664290db
unknown <870991173@qq.com>
2024-10-29 07:46:44 +0800

    recent changee

--- src/main/java/org/example/sql/DataGenTest.java
+++ src/main/java/org/example/sql/DataGenTest.java
@@ -1,12 +1,17 @@
 package org.example.sql;
 
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.types.Row;
 
 public class DataGenTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().inStreamingMode().build());
@@ -17,12 +22,19 @@ public class DataGenTest {
                 "order_time timestamp" +
                 ") " +
                 "with (" +
-                "   'connector' = 'datagen'," +
-                "   'rows-per-second' = '1'" +
+                "   'connector' = 'datagen'" + "," +
+                "   'rows-per-second' = '1'" + "," +
+                "   'fields.order_id.min' = '100'" + "," +
+                "   'fields.order_id.max' = '105'" +  "," +
+                "   'fields.price.min' = '0.0'" +  "," +
+                "   'fields.price.max' = '1000.0'" +
                 ")"
         );
 
-        TableResult resTable = tEnv.executeSql("select * from orders");
-        resTable.print();
+        Table resTable = tEnv.sqlQuery("select * from orders");
+        DataStream<Row> resStream = tEnv.toChangelogStream(resTable, Schema.newBuilder().build(), ChangelogMode.insertOnly());
+        resStream.print();
+
+        env.execute("Orders Data Generation and Query");
     }
 }
